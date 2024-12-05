271db8689678835b46f9ce223c1276de4ae9f932
Lokavit <491262164@qq.com>
2024-12-05 16:05:49 +0800

    -c text

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-
 import config from "./src/config";
 import * as number from "./src/number";
 import * as date from "./src/date";
-import * as text from "./src/text";
+import { fmtText } from "./src/text";
 
 const fmt = {
   config,
+  fmtText,
   ...number,
   ...date,
-  ...text,
 };
 
 export default fmt;
