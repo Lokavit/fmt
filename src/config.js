c17d3e56b5a02100c46c629a250e7e499a92e768
Lokavit <491262164@qq.com>
2024-12-05 15:43:30 +0800

    -c text

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
-
-
 export default {
   lgre: "en-US",
   timezone: "",
   decimal: "",
+  ignore: ["zh-TW", "ja-JP", "ko-KR", "ar-IL"],
 };
---
+++ src/text.js
@@ -0,0 +1,36 @@
+import config from "./config";
+
+// 标题或重要标签 每个单词首字母大写 （Title Case）
+export const TITLE_CASE = "title";
+// 一般描述文本 首个单词首字母大写 （Sentence Case）
+export const SENTENCE_CASE = "sentence";
+
+export const fmtText = (val, method = TITLE_CASE) => {
+  // 是否为西文
+  const isWestern = !config.ignore.includes(config.lgre);
+  // console.log(`是否西文:`, isWestern);
+  if (!isWestern) return val;
+  // 西文格式化处理 首个单词首字母大写 （Sentence Case）
+  const capitalizeFirstLetter = (str) =>
+    str.charAt(0).toUpperCase() + str.slice(1);
+  // 西文格式化处理 每个单词首字母大写 （Title Case）
+  const toTitleCase = (str) =>
+    str
+      .split(" ")
+      .map((word) => capitalizeFirstLetter(word))
+      .join(" ");
+  switch (method) {
+    // case constants.FMT_METHOD_TC:
+    // 	// 每个单词首字母大写（Title Case）
+    // 	return toTitleCase(val);
+    case SENTENCE_CASE:
+      // 首个单词首字母大写（Sentence Case）
+      return capitalizeFirstLetter(val);
+
+    // 如果有其他模式，在此追加。如，字符全小写。
+
+    default:
+      // 每个单词首字母大写（Title Case）
+      return toTitleCase(val);
+  }
+};
